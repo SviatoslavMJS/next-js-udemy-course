3fe55048e3ac89cdba91f3cc5f99b43f175c5754
Sviatoslav <toslav@ukr.net>
2024-04-03 16:18:58 +0300

    base pages

---
+++ foodie-app/app/meals/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const MealsPage = () => {
+  return (
+    <>
+      <h1>Meals Page</h1>
+      <Link href="/meals/share" >Share</Link>
+    </>
+  );
+};
+
+export default MealsPage;
